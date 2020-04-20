53a126af90bc92a66c429b1352593e5e005de93d
Nanda Gopal <ngopal253@gmail.com>
2020-04-20 17:56:27 +0530

    fixed Deprecation

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyles';
 import Header from './Header';
 import Meta from './Meta';
 
@@ -24,43 +25,19 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
-injectGlobal`
-   @font-face {
-    font-family: 'radnika_next';
-    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
-    font-weight: normal;
-    font-style: normal;
-  }
-  html{
-    box-sizing: border-box;
-    font-size: 10px;
-  }
-  *, *:before, *:after{
-    box-sizing: inherit;
-  }
-  body{
-    padding:0;
-    margin:0;
-    font-size:1.5rem;
-    line-height:2;
-    font-family:'radnika_next'
-  }
-  a{
-    text-decoration:none;
-    color:${theme.black};
-  }
-`;
-
 export default class Page extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <StyledPage>
-          <Meta />
-          <Header />
-          <Inner>{this.props.children}</Inner>
-        </StyledPage>
-      </ThemeProvider>
+      <>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <StyledPage>
+            <Meta />
+            <Header />
+            <Inner>{this.props.children}</Inner>
+          </StyledPage>
+        </ThemeProvider>
+      </>
     );
   }
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Page from '../components/Page';
 
 class MyApp extends App {
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
-      <Container>
-        <Page>
-          <Component />
-        </Page>
-      </Container>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
     );
   }
 }
